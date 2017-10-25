41adc10892e76d33d4f0bdeaaea994a8b58257ef
xiaodoufu <501804292@qq.com>
2017-10-25 09:57:25 +0800

    为什么配置成/  不能使用  /*

---
+++ 02Url_Pattern/src/main/java/com/xdf/controller/ExtensionServlet.java
@@ -0,0 +1,22 @@
+package com.xdf.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ExtensionServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("进入了ExtensionServlet====》扩展名匹配");
+        System.out.println("getContextPath====》"+req.getContextPath());
+        System.out.println("getServletPath====》"+req.getServletPath());
+    }
+}
